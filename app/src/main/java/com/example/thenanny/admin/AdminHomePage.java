32135922ce4545a6d2a73c9633037071a94b09e7
Nastya <aheter111@gmail.com>
2023-02-13 10:40:42 +0200

    admin recycler view

---
+++ app/src/main/java/com/example/thenanny/admin/AdminHomePage.java
@@ -0,0 +1,15 @@
+package com.example.thenanny.admin;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.thenanny.R;
+
+public class AdminHomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home_page);
+    }
+}
